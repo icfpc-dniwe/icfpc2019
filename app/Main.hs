b9aac3682e33fb29926b0c7a82fff4d87ed6a2ab
vuvko <notvuvko@gmail.com>
2019-06-22 01:17:39 +0300

    AStar solver

--- app/Main.hs
+++ app/Main.hs
@@ -13,6 +13,8 @@ import ICFPC2019.IO
 import ICFPC2019.Visualize
 import ICFPC2019.FastDownward
 
+import qualified ICFPC2019.Solver.AStar as SA
+
 main :: IO ()
 main = do
   [path] <- getArgs
@@ -24,11 +26,18 @@ main = do
   print rawProb
   let (prob, state) = convertProblem rawProb
   putStrLn $ showPlane $ problemMap prob
-  res <- FD.runProblem (solveProblem prob state)
+--  res <- FD.runProblem (solveProblem prob state)
+--
+--  solution <-
+--    case res of
+--      FD.Solved plan -> return $ map fromSimpleAction $ FD.totallyOrderedPlan plan
+--      _ -> fail "Couldn't find a plan!"
+
+  let res = SA.solve prob state
 
   solution <-
     case res of
-      FD.Solved plan -> return $ map fromSimpleAction $ FD.totallyOrderedPlan plan
+      Just plan -> return $ map snd plan
       _ -> fail "Couldn't find a plan!"
 
   BB.hPutBuilder stdout $ buildSolution solution
