46ee7aae8f7c6e124d46a50456f395d058df8b17
vuvko <notvuvko@gmail.com>
2019-06-22 17:37:01 +0300

    best solutions

--- submit-solutions.sh
+++ submit-solutions.sh
@@ -3,7 +3,7 @@ set -e
 
 tmpfile="$(mktemp).zip"
 (
-  cd tmp_solutions2
+  cd best_solutions
   zip "$tmpfile" *.sol
 )
 curl -F "private_id=305c47431f548d935d3d3604" -F "file=@${tmpfile}" https://monadic-lab.org/submit
