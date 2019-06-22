48700f1b385e3d7a0e50e1053feb6b1c8e13515f
Nikolay Amiantov <ab@fmap.me>
2019-06-22 12:44:03 +0300

    Fix astar

--- src/ICFPC2019/Solver/AStar.hs
+++ src/ICFPC2019/Solver/AStar.hs
@@ -1,9 +1,11 @@
 module ICFPC2019.Solver.AStar where
 
+import qualified Data.Set as S
+  
 import DNIWEChan.Graph
 import ICFPC2019.Types
 import ICFPC2019.Utils
 import ICFPC2019.Solver.Utils
 
 solve :: Problem -> ProblemState -> Maybe [(ProblemState, Action)]
-solve problem state = aStar (getNeighbours problem) diffWrapped state $ genFinish state
+solve problem state = aStar (getNeighbours problem) (S.size . problemUnwrapped) state (S.null . problemUnwrapped)
