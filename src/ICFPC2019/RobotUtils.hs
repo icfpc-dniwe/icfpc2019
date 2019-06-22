5a4d06d54098b6a31748f13f3a51b344ed4eb454
alvearian <am-vi@yandex.ru>
2019-06-22 15:45:34 +0300

    get all neighbours

--- src/ICFPC2019/RobotUtils.hs
+++ src/ICFPC2019/RobotUtils.hs
@@ -1,13 +1,11 @@
 module ICFPC2019.RobotUtils
   ( move
   , rot
-  , speed
-  , drillEnabled
-  , hasUnspentBeacons
   , checkBoundaries
   , checkObstacles
+  , manipulatorExtensionLocations
+  , validManipulators
   , applyAction
-  , validateRobot
   , applyOrientation
   , rotateLeft
   , rotateRight
@@ -17,7 +15,7 @@ import Data.Set (Set)
 import qualified Data.Set as S
 import qualified Data.HashMap.Strict as M
 import qualified Data.Array.Repa as R
-import Linear.V2 (V2(..))
+import Linear.V2 (V2(..), crossZ)
 import Data.Maybe
 
 import ICFPC2019.Types
@@ -58,6 +56,74 @@ checkBoundaries gameMap = R.inShapeRange (V2 0 0) (R.extent gameMap - 1)
 
 checkObstacles :: MapArray Cell -> I2 -> Bool
 checkObstacles gameMap pos = (gameMap R.! pos) /= Obstacle
+
+sign :: Float -> Int
+sign v
+    | abs v < 1e-10 = 0
+    | v > 0.0 = 1
+    | otherwise = -1
+
+--             * C
+--  
+--  ---*-------------*
+--     A             B
+--
+--         *D
+-- sign(AC x AB) != sign(AD x AB)
+segmentIntersectsLine :: V2 Float -> V2 Float -> I2 -> I2 -> Bool
+segmentIntersectsLine a b (V2 xc yc) (V2 xd yd) =
+    let c = V2 (fromIntegral xc) (fromIntegral yc)
+        d = V2 (fromIntegral xd) (fromIntegral yd)
+        ab = b - a :: V2 Float
+        ad = d - a :: V2 Float
+        ac = c - a :: V2 Float
+        acxab = (crossZ ac ab) :: Float
+        adxab = (crossZ ad ab) :: Float
+    in sign acxab /= sign adxab
+
+obstaclesInBoundingBox :: MapArray Cell -> I2 -> I2 -> Set I2
+obstaclesInBoundingBox map_ (V2 x1 y1) (V2 x2 y2) = 
+    let allCells = [(V2 x y) | x <- [x1..x2],
+                               y <- [y1..y2]
+                   ]
+        obstacles = filter (checkObstacles map_) allCells
+    in S.fromList obstacles
+
+-- cellToRect :: cell -> sides
+cellToRect :: I2 -> [(I2, I2)]
+cellToRect (V2 x y) =
+    let p0 = V2 x y
+        p1 = V2 x (y+1)
+        p2 = V2 (x+1) (y+1)
+        p3 = V2 (x+1) y
+    in [(p0, p1), (p1, p2), (p2, p3), (p3, p1)]
+
+--checkCellVisibility' :: src -> dst -> obstacle rect -> bool 
+checkCellVisibility' :: I2 -> I2 -> [(I2, I2)] -> Bool
+checkCellVisibility' (V2 xa ya) (V2 xb yb) sides = 
+    let a = V2 (0.5 + fromIntegral xa) (0.5 + fromIntegral ya) :: V2 Float
+        b = V2 (0.5 + fromIntegral xb) (0.5 + fromIntegral yb) :: V2 Float
+    in not $ any (\(c, d) -> segmentIntersectsLine a b c d) sides
+
+--checkCellVisibility :: map -> src -> dst -> bool
+checkCellVisibility :: MapArray Cell -> I2 -> I2 -> Bool
+checkCellVisibility map_ src@(V2 x0 y0) dst@(V2 x1 y1) = 
+    let obstacles = obstaclesInBoundingBox map_ src dst
+        obstRects = cellToRect <$> (S.toList obstacles)
+    in all id $ (checkCellVisibility' src dst) <$> obstRects
+
+manipulatorExtensionLocations' :: I2 -> Set I2
+manipulatorExtensionLocations' (V2 x y) = S.fromList [V2 (x+1) y, V2 (x-1) y, V2 x (y+1), V2 x (y-1)]
+
+manipulatorExtensionLocations :: Set I2 -> Set I2
+manipulatorExtensionLocations manips = S.difference (foldr1 (S.union) $ map manipulatorExtensionLocations' $ S.toList manips) manips
+
+--validManipulators :: map -> pivot -> manipulators -> valid manipulators
+validManipulators :: MapArray Cell -> I2 -> Set I2 -> Set I2
+validManipulators map_ pivot manips = 
+    let unfolded = filter (checkBoundaries map_) $ S.toList manips
+        visible = filter (checkCellVisibility map_ pivot) unfolded
+    in S.fromList visible
 
 decrementBoosters :: Robot -> Robot
 decrementBoosters r = r {
--- src/ICFPC2019/Solver/Utils.hs
+++ src/ICFPC2019/Solver/Utils.hs
@@ -2,6 +2,7 @@ module ICFPC2019.Solver.Utils where
 
 import qualified Data.Set as S
 import qualified Data.Array.Repa as R
+import Data.Maybe
 import Linear.V2
 import ICFPC2019.Utils
 import ICFPC2019.Types
@@ -26,6 +27,32 @@ getNeighbours problem@(Problem {..}) state@(ProblemState {..}) =
     , checkBoundaries problemMap newPos
     , checkObstacles problemMap newPos
   ]
+
+getAllNeighbours :: Problem -> ProblemState -> [(ProblemState, Action, Int)]
+getAllNeighbours problem@(Problem {..}) state@(ProblemState {..}) = 
+  let robot = problemRobot
+      map_ = problemMap
+      moves = [
+          MUp, MRight, MDown, MLeft,
+          MTurnRight, MTurnLeft,
+          MAttachWheels, MAttachDrill, MPlaceBeacon
+        ] ++ (MTeleport <$> (S.toList $ robotBeacons robot))
+          ++ (MAttachManipulator <$> (S.toList $ manipulatorExtensionLocations $ robotManipulators robot))
+      costs = [1..]
+      newRobots = (applyAction robot map_ state) <$> moves
+      validRobots = catMaybes $ map (\(mr, move, cost) -> case mr of
+                                            Just r -> Just (r, move, cost)
+                                            Nothing -> Nothing
+                                    ) $ zip3 newRobots moves costs
+      validManips r = validManipulators map_ (robotPosition r) (robotManipulators r)
+      newWrapped r = map (+ (robotPosition r)) $ S.toList $ validManips r
+      newState r move cost = (
+          state {
+            problemRobot = r,
+            problemUnwrapped = foldr S.delete problemUnwrapped $ newWrapped r
+          }, move, cost
+        )
+  in map (\(r, m, c) -> newState r m c) validRobots
 
 genFinish :: ProblemState -> ProblemState
 genFinish start@(ProblemState {..}) = start {problemUnwrapped = S.empty}
