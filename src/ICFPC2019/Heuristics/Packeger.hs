2d0f29cc0157a3ad7d4655640b7666a5e0896e1c
vuvko <notvuvko@gmail.com>
2019-06-22 15:43:40 +0300

    heuristics

---
+++ src/ICFPC2019/Heuristics/Packeger.hs
@@ -0,0 +1,32 @@
+module ICFPC2019.Heuristics.Packeger where
+
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as M
+import ICFPC2019.Types
+import ICFPC2019.Utils
+
+uncycleTrace :: Problem -> ProblemState -> [(ProblemState, Action)] -> [(ProblemState, Action)]
+uncycleTrace Problem{..} ProblemState{..} trace =
+  clearTrace trace $ mark M.empty trace 0
+    where
+      mark :: HashMap Robot (Int, Set I2) -> [(ProblemState, Action)] -> Int -> [(Int, Int)]
+      mark storage ((curState, _) : trace) curIdx =
+        let
+          curRobot = problemRobot curState
+          curUnwrapped = problemUnwrapped curState
+          storage' = M.insert curRobot (curIdx, curUnwrapped) storage
+        in
+        case M.lookup curRobot storage of
+          Just (pos, unwrapped) -> if unwrapped == curUnwrapped
+                                   then (pos, curIdx) : mark storage' trace (curIdx + 1)
+                                   else mark storage' trace (curIdx + 1)
+          _                     -> mark storage' trace (curIdx + 1)
+      clearTrace :: [a] -> [(Int, Int)] -> [a]
+      clearTrace trace marks = helper (zip [1..] trace) marks
+        where
+          helper trace [] = trace
+          helper []    _  = []
+          helper trace@((curIdx, elem):otherTrace) marks@((curBegin, curEnd):otherMarks)
+            | curIdx <= curBegin = elem : helper otherTrace marks
+            | curIdx >  curEnd   = helper trace otherMarks
+            | otherwise          = helper otherTrace marks
