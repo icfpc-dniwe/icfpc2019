30dcfa6087d348d6046cc4dd6ed4e6fee2d4c5f6
Nikolay Amiantov <ab@fmap.me>
2019-06-23 14:20:41 +0300

    Ghhh

--- horrible_things/app/index.js
+++ horrible_things/app/index.js
@@ -1,56 +1,95 @@
-'use strict;'
+"use strict"
 
 const fs = require('fs')
 const path = require('path')
+const { promisify } = require('util')
 const mime = require('mime-types')
 const jsdom = require("jsdom");
-const { addFileList } = require('addFileList')
+const _FileList = require('../node_modules/jsdom/lib/jsdom/living/generated/FileList.js')
+const jsdomUtils = require('../node_modules/jsdom/lib/jsdom/living/generated/utils.js')
 
 const { JSDOM } = jsdom;
 
-if (process.argv.length < 4)
-{
-    console.log("Usage: " + __filename + " task solution");
-    process.exit(1);
+const readFileAsync = promisify(fs.readFile);
+const statAsync = promisify(fs.stat);
+const setTimeoutAsync = promisify(setTimeout);
+
+const tryOrWait = async (cb) => {
+    while (true) {
+        await setTimeoutAsync(0);
+        const r = await cb();
+        if (r) {
+            return r;
+        }
+    }
 }
-var taskPath = process.argv[2]
-var solutionPath = process.argv[3]
-console.log("Checking task: '" + taskPath + "' solution: '" + solutionPath + "'");
 
+const main = async () => {
+    if (process.argv.length < 4)
+    {
+        console.log("Usage: " + __filename + " task solution");
+        return;
+    }
+    const taskPath = process.argv[2]
+    const solutionPath = process.argv[3]
+    console.log("Checking task: '" + taskPath + "' solution: '" + solutionPath + "'");
+    
+    const URL = "https://icfpcontest2019.github.io/solution_checker/"
+    const options = {
+        includeNodeLocations: true,
+        pretendToBeVisual: true,
+        resources: "usable",
+        runScripts: "dangerously"
+    }
 
-var URL = "https://icfpcontest2019.github.io/solution_checker/"
-var options = {
-    includeNodeLocations: true,
-    pretendToBeVisual: true,
-    resources: "usable",
-    runScripts: "dangerously"
-}
-JSDOM.fromURL(URL, options).then(dom => {
-    setTimeout(() => {
-        //console.log(dom.serialize());
+    const dom = await JSDOM.fromURL(URL, options);
+    const { window } = dom
+    const { document } = window
 
-        const { window } = dom
-        const { document } = window
+    const loadFile = async (input, filePath) => {
+        const { mtimeMs: lastModified, size } = await statAsync(filePath);
+        const text = await readFileAsync(filePath);
+        const file = new window.File(
+            [text],
+            path.basename(filePath),
+            {
+                lastModified,
+                type: mime.lookup(filePath) || '',
+            }
+        );
+        const fileListImpl = _FileList.createImpl();
+        fileListImpl.push(file);
+        const fileList = jsdomUtils.wrapperForImpl(fileListImpl);
+        _FileList.convert(fileList);
 
+        Object.defineProperty(input, 'files', {
+            value: fileList,
+            writable: false,
+        })
+    }
+
+    const result = await tryOrWait(async () => {
         const btnSubmitTask = document.getElementById("submit_task");
-        addFileList(btnSubmitTask, taskPath);
-
         const btnSubmitSolution = document.getElementById("submit_solution");
-        addFileList(btnSubmitSolution, solutionPath)
-
         const btnCheck = document.getElementById("execute_solution");
-        
+        const outputField = document.getElementById("output");
+	      if (!(btnSubmitTask && btnSubmitSolution && btnCheck && outputField)) return;
+
+        await loadFile(btnSubmitTask, taskPath);
         btnSubmitTask.click();
+
+        await loadFile(btnSubmitSolution, solutionPath);
         btnSubmitSolution.click();
 
-        setTimeout(() => {
-            btnCheck.click();
-            setTimeout(() => {
-                const outputField = document.getElementById("output");
-                console.log(dom.serialize());
-                console.log(outputField.textContent);
-            }
-            , 5000);
-        }, 1000);
-    }, 1000)
-});
+        btnCheck.click();
+
+        return await tryOrWait(async () => {
+            if (outputField.textContent.length === 0) return;
+            return outputField.textContent;
+        })
+    });
+
+    console.log(result);
+}
+
+main()
